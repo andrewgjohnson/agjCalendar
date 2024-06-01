6e2333922615a1d50438ceaa1dfc26d2796af0db
Andrew G. Johnson <andrew@andrewgjohnson.com>
2024-06-01 13:42:29 -0500

    Fixed typo

--- examples/07-included-and-custom-themes-2.js
+++ examples/07-included-and-custom-themes-2.js
@@ -1,4 +1,4 @@
-// Integrations using a custom theme
+// Integration using a custom theme
 $.agjCalendar({
   dateSelector: '#example-07-2-custom',
   theme:        'custom-xxx' // Custom themes must begin with 'custom-'
